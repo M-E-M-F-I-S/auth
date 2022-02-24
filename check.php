60701f6788755b37389b863351ce54eb1f0ad6cd
Vod1chka <99795108+Vod1chka@users.noreply.github.com>
2022-02-24 21:51:25 +0800

    Delete check.php

--- check.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = filter_var(trim($_POST['username']),FILTER_SANITIZE_STRING);
-$password = filter_var(trim($_POST['password']),FILTER_SANITIZE_STRING);
-
-if (mb_strlen($username) < 5 || mb_strlen($username) > 90){
-	echo 'Недопустимая длинна логина';
-	exit();
-}
-?>
