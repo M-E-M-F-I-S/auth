4b06dcb052447913cc2ecafa9189919040dce45f
Vod1chka <99795108+Vod1chka@users.noreply.github.com>
2022-02-24 23:58:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang="ru">
+	<head>
+	<meta charset="utf-8"> 
+ <title>Главная</title>	
+<style>
+p { 
+	font-size: 15pt;
+	font-family: Arial;
+}
+</style>
+</head>
+<body>
+	<p>
+	<?php echo'Вы вошли на сайт как гость, <p>пожалуйста <a href= "login.php">авторизуйтесь</a> или <a href= "register.php">создайте аккаунт</a>!</p>'?>
+	</p>
+</body>
+</html>
