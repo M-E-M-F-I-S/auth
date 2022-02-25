936ca43a852055a5ac62fb9eeb3bfcaa0bce536a
Aleksandr Dmitrienko <dmitrienko.alexander@gmail.com>
2022-02-25 15:27:16 +0800

    Update auth.php

--- validation-form/auth.php
+++ validation-form/auth.php
@@ -1,26 +1,19 @@
 <?php
-	$username = filter_var(trim($_POST['username']),
-	FILTER_SANITIZE_STRING);
-	$password = filter_var(trim($_POST['password']),
-	FILTER_SANITIZE_STRING);
+require_once '../database/db.php';
 
-    $password = md5($password."54uy217sgh");
+$username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
+$password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
+$salt 	  = "54uy217sgh";
+$password = md5($password.$salt);
     
-    require 'E:/Work/php learning/database/db.php';
-
-
-
-    $results = $mysql->query("SELECT * FROM `myguests` WHERE `username` ='$username' AND `password` = '$password'");
-    $user = $results -> fetch(PDO::FETCH_ASSOC);
-
+$results = $mysql->query("SELECT * FROM `myguests` WHERE `username` = '$username' AND `password` = '$password'");
+$user 	 = $results->fetch(PDO::FETCH_ASSOC);
 
 if(count($user) == 0 ) {
-	echo "Неверный логин или пароль";
-	exit();
+    echo "Неверный логин или пароль";
+    exit();
+} else {
+    echo ("Вы успешно авторизовались");
 }
-else echo ("Вы успешно авторизовались");
-
-
-
 
 ?>
