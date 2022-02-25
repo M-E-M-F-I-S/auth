eed42980bce174aba913da37c99a040620b32ed7
Aleksandr Dmitrienko <dmitrienko.alexander@gmail.com>
2022-02-25 15:52:31 +0800

    Update createtable.php
    Подключение файла с конфигами

--- database/createtable.php
+++ database/createtable.php
@@ -1,30 +1,25 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'myDBPDO';
-
+require_once 'config.php';
 
 try {
-$conn = new PDO ("mysql:host = $servername;dbname=$dbname" , $username,$password);
-$conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO ("mysql:host = $servername;dbname=$dbname" , $username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    /* Создание таблицы */
+    $sql = "CREATE TABLE MyGuests (
+	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	firstname varchar(32) collate utf8_unicode_ci NOT NULL,
+	email varchar(32) collate utf8_unicode_ci NOT NULL,
+	username varchar(20) collate utf8_unicode_ci NOT NULL,
+	password varchar(32) collate utf8_unicode_ci NOT NULL,
+	UNIQUE KEY username (username)
+    )";
+    $conn->exec($sql);
 
-/* Создание таблицы */
-$sql = "CREATE TABLE MyGuests (
-id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-firstname varchar(32) collate utf8_unicode_ci NOT NULL,
-email varchar(32) collate utf8_unicode_ci NOT NULL,
-username varchar(20) collate utf8_unicode_ci NOT NULL,
-password varchar(32) collate utf8_unicode_ci NOT NULL,
-UNIQUE KEY username (username)
-)";
-
-/* Вывод сообщения об успехе */
-$conn->exec($sql);
-echo "Table MyGuests created succesfully";
-}catch (PDOException $e) {
-	echo $sql . "<br>" . $e->getMessage();
+    /* Вывод сообщения об успехе */
+    echo "Table MyGuests created succesfully";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
 }
 
 $conn= null;
