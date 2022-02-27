a93e8edfd5733ec9e1209514376183f21708bbf5
Aleksandr Dmitrienko <3970002+M-E-M-F-I-S@users.noreply.github.com>
2022-02-27 12:36:32 +0800

    Update save_names.php
    1) Разделяй логику на небольшие блоки, комментируй каждый из них для удобства. Каждый блок выполняется только если предыдущий был успешен. Например если данные не валидны - то можно их уже и не записывать ни в какие переменные.
    2) Старайся не смешивать получение/валидацию и очистку данных, это операции которые идут бок о бок, но тебе как разработчику гораздо понятнее работать с блоком, в котором выполняется что-то одно
    3) Можно использовать вложенные функции, например trim(htmlspecialchars(stripslashes($_POST['firstname']))); 
    4) Чем меньше запросов к базе - тем лучше, поэтому объединяй данные где это возможно - на примере проверке существования пользователя

--- validation-form/save_names.php
+++ validation-form/save_names.php
@@ -6,49 +6,36 @@
    </head>
 
 <?php
-   if (isset($_POST['firstname'])) { $firstname = $_POST['firstname']; if ($firstname == '') { unset($firstname);} }
-   if (isset($_POST['email'])) { $email=$_POST['email']; if ($email =='') { unset($email);} }
-   if (isset($_POST['username'])) { $username=$_POST['username']; if ($username == '') { unset($username);} }
-   if (isset($_POST['password'])) { $password=$_POST['password']; if ($password == '') { unset($password);} }
- if (empty($firstname) or empty($email) or empty ($username) or empty ($password))
-    {
-    exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
+    require_once '../database/config.php';
+    require_once '../database/db.php';      
+      
+    //валидация данных  
+    if (!isset($_POST['firstname']) || !isset($_POST['email']) || !isset($_POST['username']) || !isset($_POST['password'])) {
+        exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
     }
+      
+    //очистка данных
+    $firstname = trim(htmlspecialchars(stripslashes($_POST['firstname'])));      
+    $email     = trim(htmlspecialchars(stripslashes($_POST['email'])));
+    $username  = trim(htmlspecialchars(stripslashes($_POST['username'])));
+    $password  = trim(htmlspecialchars(stripslashes($_POST['password'])));
     
-    $firstname = stripslashes($firstname);
-    $firstname = htmlspecialchars($firstname);
-    $email = stripslashes($email);
-    $email = htmlspecialchars($email);
-    $username = stripslashes($username);
-    $username = htmlspecialchars($username);
-    $password = stripslashes($password);
-    $password = htmlspecialchars($password);
-    $firstname = trim($firstname);
-    $username = trim($username);
-    $password = trim($password);
-
-
-    $password = md5($password."54uy217sgh");
-    require 'E:/Work/php learning/database/db.php';
-    $results = $mysql->query("SELECT * FROM `myguests` WHERE `username` ='$username'");
-    $myrow = $results -> fetch(PDO::FETCH_ASSOC);
-    if (!empty($myrow['id'])){
-      exit ("Извините, введёный вами логин уже зарегистрирован, введите другой логин");
-    }
-   $results = $mysql->query("SELECT * FROM `myguests` WHERE `email` ='$email'");
-   $myrow = $results -> fetch(PDO::FETCH_ASSOC);
-    if (!empty($myrow['id'])){
-      exit ("Извините, введённая вами электронная почта уже зарегистрирована.");
+    //проверяем на существование пользователя
+    $results = $mysql->query("SELECT * FROM `myguests` WHERE `username` = '$username' OR `email` = '$email'");
+    $myrow   = $results->fetch(PDO::FETCH_ASSOC);
+    if (!empty($myrow['id'])) {
+       if ($myrow['username'] === $username) {
+          exit ("Извините, введёный вами логин уже зарегистрирован, введите другой логин");
+       } else {
+          exit ("Извините, введённая вами электронная почта уже зарегистрирована.");
+       }
     }
 
-
-    $sql = 'INSERT INTO MyGuests (firstname,email,username,password)
-    VALUES (:firstname,:email,:username,:password)';
-    $query = $mysql->prepare($sql);
-    $query->execute(['firstname' =>$firstname,'email' =>$email,'username' =>$username,'password' =>$password]);
+    //сохраняем нового пользователя
+    $password = md5($password."54uy217sgh"); 
+    $sql      = 'INSERT INTO MyGuests (firstname,email,username,password) VALUES (:firstname,:email,:username,:password)';
+    $query    = $mysql->prepare($sql);
+    $query->execute(['firstname' => $firstname,'email' => $email,'username' => $username,'password' => $password]);
 
     echo "Вы успешно зарегистрированы! Теперь вы можете зайти на сайт. <a href='/login.php'>Авторизация</a>";
- //else {
-   // echo "Ошибка! Вы не зарегистрированы.";
-    //}
-    ?>
+?>
